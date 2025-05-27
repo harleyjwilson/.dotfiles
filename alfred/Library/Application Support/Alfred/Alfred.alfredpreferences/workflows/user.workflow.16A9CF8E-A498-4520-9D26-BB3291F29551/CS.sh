3c69bf671206b237eea7060f2ab373b34ea63b33
Harley Wilson <54230223+harleyjwilson@users.noreply.github.com>
2025-05-27 10:59:50 +0800

    Update Alfred config

---
+++ alfred/Library/Application Support/Alfred/Alfred.alfredpreferences/workflows/user.workflow.16A9CF8E-A498-4520-9D26-BB3291F29551/CS.sh
@@ -0,0 +1,114 @@
+cat <<EOF
+{
+  "items": [
+    {
+      "title": "All-in-one mode",
+      "subtitle": "Choose your CleanShot utility on screen",
+      "arg": "all-in-one",
+      "icon": {
+        "path": "./Images/All-in-one mode.png"
+      },
+      "uid": "all-in-one-mode"
+    },
+    {
+      "title": "Annotate clipboard image",
+      "subtitle": "Annotate an image from the clipboard",
+      "arg": "open-from-clipboard",
+      "icon": {
+        "path": "./Images/Annotate.png"
+      },
+      "uid": "Annotate clipboard image"
+    },
+    {
+      "title": "Open Annotate",
+      "subtitle": "Open the Annotate utility",
+      "arg": "open-annotate",
+      "icon": {
+        "path": "./Images/Annotate.png"
+      },
+      "uid": "Open Annotate"
+    },
+    {
+      "title": "Area capture",
+      "subtitle": "Take a screenshot of a selected area on screen",
+      "arg": "capture-area",
+      "icon": {
+        "path": "./Images/Capture area.png"
+      },
+      "uid": "Area capture"
+    },
+    {
+      "title": "Area capture with self-timer",
+      "subtitle": "Capture a selected area using self-timer",
+      "arg": "self-timer",
+      "icon": {
+        "path": "./Images/Self-timer.png"
+      },
+      "uid": "Area capture with self-timer"
+    },
+    {
+      "title": "Full-screen capture",
+      "subtitle": "Take a screenshot of the full screen",
+      "arg": "capture-fullscreen",
+      "icon": {
+        "path": "./Images/Capture full screen.png"
+      },
+      "uid": "Full-screen capture"
+    },
+    {
+      "title": "Open history",
+      "subtitle": "Open CleanShot history",
+      "arg": "open-history",
+      "icon": {
+        "path": "./Images/Capture history.png"
+      },
+      "uid": "Open history"
+    },
+    {
+      "title": "Previous area capture",
+      "subtitle": "Take a screenshot using the previous area",
+      "arg": "capture-previous-area",
+      "icon": {
+        "path": "./Images/Capture previous area.png"
+      },
+      "uid": "Previous area capture"
+    },
+    {
+      "title": "Record screen",
+      "subtitle": "Make a recording of the screen",
+      "arg": "record-screen",
+      "icon": {
+        "path": "./Images/Record screen.png"
+      },
+      "uid": "Record screen"
+    },
+    {
+      "title": "Scrolling capture",
+      "subtitle": "Take a screenshot of a scrolling area",
+      "arg": "scrolling-capture",
+      "icon": {
+        "path": "./Images/Scrolling capture.png"
+      },
+      "uid": "Scrolling capture"
+    },
+    {
+      "title": "Text capture",
+      "subtitle": "Capture text (OCR)",
+      "arg": "capture-text",
+      "icon": {
+        "path": "./Images/Capture text.png"
+      },
+      "uid": "Text capture"
+    },
+    {
+      "title": "Window capture",
+      "subtitle": "Take a screenshot of a window",
+      "arg": "capture-window",
+      "icon": {
+        "path": "./Images/Capture window.png"
+      },
+      "uid": "Window capture"
+    }
+  ]
+}
+EOF
