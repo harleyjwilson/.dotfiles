f20a6917929af67b45482c0bfb7ad5b9e8062de1
Harley Wilson <54230223+harleyjwilson@users.noreply.github.com>
2025-07-09 16:49:50 +0800

    Update nvim config

--- nvim/.config/nvim/lua/config/autocommands.lua
+++ nvim/.config/nvim/lua/config/autocommands.lua
@@ -5,11 +5,18 @@
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Highlight when yanking (copying) text",
-	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+-- Turn on line wrapping for .md and .txt files
+vim.api.nvim_create_autocmd('BufEnter', {
+  pattern = { '*.md', '*.txt' },
+  group = group,
+  command = 'setlocal wrap'
 })
 
 -- vim: ts=2 sts=2 sw=2 et
